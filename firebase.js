dadd870cbaef313694c33fd76669cebf0eccb196
inves13 <investh13@gmail.com>
2025-04-25 17:45:41 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js';
-import { getDatabase, ref, get, set } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js';
-import { getAuth } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js';
+import { getDatabase, ref, get, set, child, update } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js';
 
 // Configuração do Firebase
 const firebaseConfig = {
@@ -18,6 +18,7 @@ const app = initializeApp(firebaseConfig);
 
 // Obtém uma referência ao banco de dados
 const database = getDatabase(app);
+const auth = getAuth();
 
 // Função para obter dados do usuário
 function obterDadosUsuario(telefoneUsuario, callback) {
@@ -46,4 +47,41 @@ function salvarCodigoConvite(telefone, codigo) {
     });
 }
 
-export { obterDadosUsuario, salvarCodigoConvite };
+// Função para criar um novo usuário
+function cadastrarUsuario(telefone, senha, codigoConvite) {
+  createUserWithEmailAndPassword(auth, telefone + "@email.com", senha)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      
+      // Agora salva os dados do usuário no banco de dados
+      const usuarioRef = ref(database, "usuarios/" + telefone);
+      set(usuarioRef, {
+        nome: telefone, // Adapte conforme necessário
+        codigo_convite: codigoConvite,
+        saldo: 0
+      })
+      .then(() => {
+        console.log("Usuário cadastrado com sucesso!");
+      })
+      .catch((error) => {
+        console.error("Erro ao salvar dados do usuário:", error);
+      });
+    })
+    .catch((error) => {
+      console.error("Erro ao criar usuário:", error);
+    });
+}
+
+// Função para atualizar dados de um usuário (exemplo: adicionar saldo)
+function atualizarDadosUsuario(telefone, novosDados) {
+  const usuarioRef = ref(database, "usuarios/" + telefone);
+  update(usuarioRef, novosDados)
+    .then(() => {
+      console.log("Dados do usuário atualizados com sucesso!");
+    })
+    .catch((error) => {
+      console.error("Erro ao atualizar dados:", error);
+    });
+}
+
+export { obterDadosUsuario, salvarCodigoConvite, cadastrarUsuario, atualizarDadosUsuario };
